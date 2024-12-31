e6bf5d4b871e7b1e51e61fdaf1d8764b8c84edef
kay-too <mathaiskoulloupias@gmail.com>
2024-12-31 17:53:15 +0000

    asteroid splitting

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.collidesWith(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
 
         for obj in drawable:
